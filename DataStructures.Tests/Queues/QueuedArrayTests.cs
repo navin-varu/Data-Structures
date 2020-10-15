f07c18938cd24293c05ac606f3464f2b9ce631eb
navin-varu <navin.varu31@gmail.com>
2020-10-15 20:11:22 +0530

    DataStructures Unit Test project created.

---
+++ DataStructures.Tests/Queues/QueuedArrayTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using DataStructures.Queues;
+
+namespace DataStructures.Tests.Queues
+{
+    public class QueuedArrayTests
+    {
+       
+        [Test]
+        public static void ClearQualityTest()
+        {
+            // Arrange
+            //var queue = new QueuedArray();
+
+            // Act
+
+            // Assert
+        }
+
+        [Test]
+        public static void DequeueQualityTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Test]
+        public static void DisplayQualityTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Test]
+        public static void Enqueue(string item)
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Test]
+        public static void IsEmptyQualityTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Test]
+        public static void IsFullQualityTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Test]
+        public static void PeakQualityTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Test]
+        public static void GetEnumeratorQualityTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
